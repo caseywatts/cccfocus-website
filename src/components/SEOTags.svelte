438cb3e916e5139dfbf38bc492e03d233ca09e06
Casey Watts <casey.s.watts@gmail.com>
2024-07-23 11:03:14 -0400

    SEO Tags structure set up

---
+++ src/components/SEOTags.svelte
@@ -0,0 +1,42 @@
+<script>
+  import defaultImage from "../../public/assets/cccfocus-logo.png";
+  export let title = "";
+  export let author = "Charm City Community Focus";
+  export let url = "";
+  export let description = "";
+  export let image = defaultImage;
+  export let contentType = "website";
+  export let twitterCreator = "@heycaseywattsup";
+</script>
+
+<!-- Regular -->
+<title>{title}</title>
+<link rel="canonical" href={url} />
+<meta name="title" content={title} />
+<meta name="author" content={author} />
+<meta name="url" content={url} />
+<meta name="description" content={description} />
+<meta name="image" content={image} />
+
+<!-- OpenGraph -->
+<meta property="og:title" content={title} />
+<meta property="og:url" content={url} />
+<meta property="og:description" content={description} />
+<meta property="og:image" content={image} />
+<!-- <meta name="image" property="og:image:alt" content={imageDescription}> -->
+<meta property="og:type" content={contentType} />
+
+<!-- Combo for LinkedIn -->
+<meta name="title" property="og:title" content={title} />
+<meta name="url" property="og:url" content={url} />
+<meta name="description" property="og:description" content={description} />
+<meta name="image" property="og:image" content={image} />
+<!-- <meta name="image" property="og:image:alt" content={imageDescription}> -->
+<!-- <meta name="type" property="og:type" content={contentType} /> -->
+
+<!-- Twitter -->
+<meta name="twitter:title" content={title} />
+<meta name="twitter:image" content={image} />
+<!-- <meta name="twitter:site" content={url}> -->
+<meta name="twitter:description" content={description} />
+<meta name="twitter:creator" content={twitterCreator} />
